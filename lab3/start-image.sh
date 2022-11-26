f921a2a35d7f9bdd5b9c6f3009b6c986b14ef1a4
Linus Östlund <linusost@kth.se>
2022-11-26 12:22:02 +0100

    Minor fixes in the notebook and update start-image.sh

--- lab3/start-image.sh
+++ lab3/start-image.sh
@@ -1,1 +1,1 @@
-docker run -p 10000:8888 --name pyspark-notebook --mount type=bind,source="$(pwd)"/data,target=/data jupyter/pyspark-notebook:latest
+docker run -p 10000:8888 --name pyspark-notebook-1 --mount type=bind,source="$(pwd)"/data,target=/data jupyter/pyspark-notebook:latest
